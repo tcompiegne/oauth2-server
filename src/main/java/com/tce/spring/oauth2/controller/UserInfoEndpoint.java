3c5ca588e8bbbe23923635421cdd4ab22a9c6839
Titouan COMPIEGNE <titouan.compiegne@gmail.com>
2015-04-03 20:06:39 +0200

    Fix missing spring beans + change userinfoendpoint information

--- src/main/java/com/tce/spring/oauth2/controller/UserInfoEndpoint.java
+++ src/main/java/com/tce/spring/oauth2/controller/UserInfoEndpoint.java
@@ -2,6 +2,7 @@ package com.tce.spring.oauth2.controller;
 
 import java.security.Principal;
 
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,8 +12,9 @@ public class UserInfoEndpoint {
 
 	@RequestMapping(value = "/userinfo")
 	@ResponseBody
-	public Principal loginInfo(Principal principal) {
-		return principal;
+	public Object loginInfo(Principal principal) {
+		OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) principal;
+		return oAuth2Authentication.getUserAuthentication().getPrincipal();
 	}
 
 }
